c28fdda1798e73d7efed0565013e850ebac92180
Ainur <rizvanov_ainur@mail.ru>
2023-04-18 10:17:18 +0300

    Создали сайт для добавления товара, направление на страницу добавления товара в админке, путь для содежрания фотографий, репозиторий категорий (к БД пока не подключены)

--- src/main/java/com/example/final_proj/controllers/AdminController.java
+++ src/main/java/com/example/final_proj/controllers/AdminController.java
@@ -1,12 +1,28 @@
 package com.example.final_proj.controllers;
 
+import com.example.final_proj.models.Product;
+import com.example.final_proj.repository.CategoryRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
+
+    private CategoryRepository categoryRepository;
+
+    public AdminController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
     @GetMapping("/admin")
     public String admin(){
         return "admin";
+    }
+    @GetMapping("/admin/product/add")
+    public String addProduct(Model model){
+        model.addAttribute("product", new Product());
+        model.addAttribute("category", categoryRepository.findAll());
+        return "product/addProduct";
     }
 }
--- src/main/java/com/example/final_proj/controllers/AuthenticationController.java
+++ src/main/java/com/example/final_proj/controllers/AuthenticationController.java
@@ -1,13 +1,16 @@
 package com.example.final_proj.controllers;
 
-import com.example.final_proj.models.Person;
+
+import com.example.final_proj.models.Product;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+
 
 @Controller
 public class AuthenticationController {
+
     @GetMapping("/authentication")
     public String login(){
         return "authentication";
