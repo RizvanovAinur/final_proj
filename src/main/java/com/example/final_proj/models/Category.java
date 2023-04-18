80f6137bc2075ba81c760f00285e67557cae0e7e
Ainur <rizvanov_ainur@mail.ru>
2023-04-18 08:56:53 +0300

    Добавлена модель товара

---
+++ src/main/java/com/example/final_proj/models/Category.java
@@ -0,0 +1,7 @@
+package com.example.final_proj.models;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Category {
+}
---
+++ src/main/java/com/example/final_proj/models/Product.java
@@ -0,0 +1,29 @@
+package com.example.final_proj.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+
+@Entity
+public class Product {
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotEmpty(message="Наименование не может быть пустым")
+    @Column(name="title", nullable = false, columnDefinition = "text", unique = true)
+    private String title;
+    @Column(name="description", nullable = false, columnDefinition = "text")
+    @NotEmpty(message="Описание товара не может быть пустым")
+    private String description;
+    @Column(name="price", nullable = false)
+    @Min(value=1, message="Цена не может быть меньше 1 рубля")
+    private float price;
+    @Column(name="warehouse", nullable = false)
+    @NotEmpty(message = "Склад по нахождению товара не может быть пустым")
+    private String warehouse;
+
+    @Column(name="seller", nullable = false)
+    @NotEmpty(message="Информация о продавце не может быть пустым")
+    private String seller;
+}
